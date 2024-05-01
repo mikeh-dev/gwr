06cffb44d677e84ee1f7725bd1aac8cba3d01b65
mikeh-dev <mikeh112@hotmail.com>
2024-05-01 14:29:40 +0100

    Added property associations and User model enums

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+  belongs_to :landlord, class_name: 'User', foreign_key: 'owner_id'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_person_name
   has_one_attached :avatar
 
+  enum role: [admin: 0, landlord: 1, tenant: 2]
+
+  has_many :properties, foreign_key: 'owner_id'
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
