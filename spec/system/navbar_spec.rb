94f0f4c7eeafce039b33d5a8eeb403e3d7cadfab
mikeh-dev <mikeh112@hotmail.com>
2024-05-14 14:42:18 +0100

    Added Navbar spec for both logged in and logged out user

--- spec/system/navbar_spec.rb
+++ spec/system/navbar_spec.rb
@@ -1,31 +1,62 @@
 require 'rails_helper'
 
-RSpec.describe "Navbar", type: :system do
+RSpec.feature "Navigation", type: :feature do
+  let(:user) { create(:user) }
 
-  before do
-    page.driver.browser.manage.window.resize_to(1280, 1024)  # Set to desktop size
-  end  
-  
-  scenario "when user is logged out" do
-    visit root_path
-    expect(page).to have_link("Sign in", href: new_user_session_path)
-    expect(page).to have_link("Get started", href: new_user_registration_path)
-    expect(page).to have_link("Sign Up Now", href: new_user_registration_path)
-    expect(page).to have_content(svg_icon("logo", href: root_path))
+  context "when user is signed in" do
+    before do
+      login_as user
+      visit root_path
+    end
+
+    it "displays the admin dashboard link with the SVG logo" do
+      expect(page).to have_link(href: admin_dashboard_path) do |link|
+        expect(link).to have_selector("svg.text-zinc-900.fill-current.w-10.h-10")
+        expect(link).to have_selector("span.sr-only", text: "GoodWillRenting")
+      end
+    end
+
+    it "displays the mobile navigation button" do
+      expect(page).to have_selector('button[data-action="click->nav#toggle"]')
+    end
+
+    it "displays the user avatar" do
+      if user.avatar.attached?
+        expect(page).to have_selector("img[src*='#{user.avatar.filename}']")
+      end
+    end
+
+    it "displays the dropdown menu with links" do
+      expect(page).to have_button("dropdown-menu", exact: true)
+    
+      expect(page).to have_link("Inbox", href: admin_inbox_path)
+      expect(page).to have_link("Manage properties", href: admin_properties_path)
+      expect(page).to have_link("Add booking", href: admin_new_booking_path)
+      expect(page).to have_link("Account", href: edit_user_registration_path)
+      expect(page).to have_button("Sign out", exact: true)
+    end
   end
 
-  let(:user) { create(:user) }  # Ensure you have a user factory
+  context "when user is not signed in" do
+    before do
+      visit root_path
+    end
 
-  scenario "when user is signed in" do
-    login_as(user, scope: :user)
-    visit root_path
+    it "displays the root path link with the SVG logo" do
+      expect(page).to have_link(href: root_path) do |link|
+        expect(link).to have_selector("svg.text-zinc-900.fill-current.w-10.h-10")
+      end
+    end
 
-    expect(page).to have_link("Inbox", href: admin_inbox_path)
-    expect(page).to have_button("Sign out", visible: :all)
-    expect(page).not_to have_link("Sign in")
-    expect(page).not_to have_link("Get started")
-    expect(page).not_to have_link("Sign Up Now")
-    expect(page).to have_link("Profile", href: user_path(user))
-    expect(page).to have_content("logo", href: root_path)
+    it "displays sign in and sign up links" do
+      expect(page).to have_link("Sign in", href: new_user_session_path)
+      expect(page).to have_link("Get started", href: new_user_registration_path)
+    end
+
+    it "displays primary navigation links" do
+      expect(page).to have_link("Features", href: "#")
+      expect(page).to have_link("Pricing", href: "#")
+      expect(page).to have_link("About", href: "#")
+    end
   end
 end
