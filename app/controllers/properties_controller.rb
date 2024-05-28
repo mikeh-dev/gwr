39ac92da5d5270da74cedb85560c0481710631be
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-05-28 23:56:47 +0100

    Small addition to allow tenant o view the property's theyhave agreements on in their property index

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -7,7 +7,7 @@ class PropertiesController < ApplicationController
     if current_user.admin?
       @properties = Property.all
     else
-      @properties = Property.where(owner_id: current_user.id)
+      @properties = Property.where(owner_id: current_user.id).or(Property.where(id: Agreement.where(tenant_id: current_user.id).pluck(:property_id)))
     end
   end
 
