800c280847c599e44e2099f2fd4abbe3cb2da785
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-05-31 23:28:48 +0100

    Moved property and agreement methods to app controller to enable app wide

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -7,9 +7,7 @@ class AgreementsController < ApplicationController
 
   def index
     @agreements = policy_scope(Agreement)
-    @current_agreements = Agreement.where("end_date >= ? AND (landlord_id = ? OR tenant_id = ?)", Date.today, current_user.id, current_user.id)
-    @upcoming_renewals = @current_agreements.select { |agreement| agreement.renewal_date <= Date.today + 90 }
-    @expired_agreements = Agreement.where("end_date < ? AND (landlord_id = ? OR tenant_id = ?)", Date.today, current_user.id, current_user.id)
+    load_properties_and_agreements
   end
 
   def show
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,25 @@ class ApplicationController < ActionController::Base
     redirect_to(request.referrer || root_path)
   end
 
+  def load_properties_and_agreements
+    if current_user.admin?
+      @properties = Property.all
+      @agreements = Agreement.all
+    else
+      @agreements_as_landlord = current_user.agreements_as_landlord.exists? ? current_user.agreements_as_landlord : []
+      @agreements_as_tenant = current_user.agreements_as_tenant.exists? ? current_user.agreements_as_tenant : []
+      @agreements = @agreements_as_landlord + @agreements_as_tenant
+
+      @current_agreements = Agreement.where("end_date >= ? AND (landlord_id = ? OR tenant_id = ?)", Date.today, current_user.id, current_user.id)
+      @upcoming_renewals = @current_agreements.select { |agreement| agreement.renewal_date <= Date.today + 90 }
+      @expired_agreements = Agreement.where("end_date < ? AND (landlord_id = ? OR tenant_id = ?)", Date.today, current_user.id, current_user.id)
+
+      owned_property_ids = Property.where(owner_id: current_user.id).pluck(:id)
+      tenant_property_ids = Agreement.where(tenant_id: current_user.id)
+                                     .where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
+                                     .pluck(:property_id).uniq
+
+      @properties = Property.where(id: (owned_property_ids + tenant_property_ids).uniq).includes(:agreements)
+    end
+  end
 end
