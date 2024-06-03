d252e8c00285bad95d1cee887121ace9573bc2f2
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-06-03 10:27:32 +0100

    Building agreement policy spec

--- spec/policies/agreement_policy_spec.rb
+++ spec/policies/agreement_policy_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AgreementPolicy, type: :policy do
+    let(:admin) { create(:user, role: 'admin') }
+    let(:landlord) { create(:user, role: 'landlord') }
+    let(:tenant) { create(:user, role: 'tenant') }
+    let(:agreement) { create(:agreement, landlord: landlord, tenant: tenant, property: property) }
+    let(:property) { create(:property, landlord: landlord) }
   
+    subject { described_class.new(user, agreement ) }
+  
+    context 'as an admin' do
+      let(:user) { admin }
+
+        it { is_expected.to permit_actions(%i[create show index update destroy edit]) }
+    end
 end
