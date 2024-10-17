fdf8c373b4339f1f785201608c94f5b383835e11
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-17 22:41:03 +0100

    Update to host settings in prod file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.hosts = ec2-107-23-90-171.compute-1.amazonaws.com
+  config.hosts << "ec2-107-23-90-171.compute-1.amazonaws.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
